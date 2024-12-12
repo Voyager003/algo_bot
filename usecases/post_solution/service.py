2122c6faf278e4549835cb84fc517f39fc9a6566
voyager003 <miraclex000@gmail.com>
2024-12-12 12:08:10 +0900

    refactor: PR 메시지 변경
    이모지 위치 수정

--- usecases/post_solution/service.py
+++ usecases/post_solution/service.py
@@ -55,13 +55,13 @@ def handle_submission(body, view, client, needs_review):
             send_public_message(
                 client=client,
                 channel=CHANNEL_ID,
-                message=f"<@{body['user']['id']}> 님이 오늘의 풀이를 공유해주셨어요\n[{language}] {problem_name}\n\":speech_balloon: {submission_comment}\"\n:white_check_mark: 리뷰도 함께 부탁하셨어요! ({pr_url})"
+                message=f"<@{body['user']['id']}> 님이 오늘의 풀이를 공유해주셨어요\n[{language}] {problem_name}\n:speech_balloon: \"{submission_comment}\"\n:white_check_mark: 리뷰도 함께 부탁하셨어요! ({pr_url})"
             )
         else:
             send_public_message(
                 client=client,
                 channel=CHANNEL_ID,
-                message=f"<@{body['user']['id']}> 님이 오늘의 풀이를 공유해주셨어요!\n[{language}] {problem_name} \n\":speech_balloon: {submission_comment}\""
+                message=f"<@{body['user']['id']}> 님이 오늘의 풀이를 공유해주셨어요!\n[{language}] {problem_name} \n:speech_balloon: \"{submission_comment}\""
             )
 
     except Exception as e:
