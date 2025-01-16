266241b63a3a7413dc835ea0e841b72fb1b48258
voyager003 <miraclex000@gmail.com>
2025-01-16 15:00:50 +0900

    feat: modal 전환 지연시간 추가
    issue: https://github.com/Voyager003/algo_bot/issues/13

--- usecases/post_solution/applications.py
+++ usecases/post_solution/applications.py
@@ -2,6 +2,8 @@ from usecases.post_solution.service import handle_submission
 from utils.error_handler import print_error
 from usecases.post_solution.modals import show_select_review_required_modal, show_post_solution_with_review_modal, show_post_solution_without_review_modal
 
+import time
+
 def init_solution_handlers(app):
     @app.command("/알고풀이")
     def handle_submit_command(ack, body, client):
@@ -17,10 +19,15 @@ def init_solution_handlers(app):
         ack()
         need_review = body["view"]["state"]["values"]["need_review"]["review_select"]["selected_option"]["value"] == "yes"
 
-        if need_review:
-            show_post_solution_with_review_modal(body, client, callback_id="submission_with_review")
-        else:
-            show_post_solution_without_review_modal(body, client, callback_id="submission_without_review")
+        try:
+            time.sleep(1.5)
+
+            if need_review:
+                show_post_solution_with_review_modal(body, client, callback_id="submission_with_review")
+            else:
+                show_post_solution_without_review_modal(body, client, callback_id="submission_without_review")
+        except Exception as e:
+            print_error(body, client, f"Modal 전환 중 오류가 발생했습니다: {str(e)}")
 
     @app.view("submission_with_review")
     def handle_review_submission(ack, body, view, client):
